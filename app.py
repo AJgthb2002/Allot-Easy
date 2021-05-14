df8144f872f15c8fb7151a9e09e8d27e37069c9c
Ananya Joshi <73184612+AJgthb2002@users.noreply.github.com>
2021-05-14 18:20:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from menu import Menu
 
 
 mymenu= Menu()  # obj of class Menu
-header_img = open('images\header_new.jpg', 'rb').read()
+header_img = open('images/header_new.jpg', 'rb').read()
 app= Flask(__name__)
 
 def main_func():
